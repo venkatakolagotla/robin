9f06e9059fb3828b38dfc15e265474e2139db44b
venkatakolagotla <venkata.kolagotla@gmail.com>
2019-12-09 19:30:49 +0100

    Print image type for debug

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -5,6 +5,7 @@ import cv2
 
 def test_dataset():
     img = cv2.imread('test_data/input_imgs/02_in.png')
+    print("---------------------------------", type(img))
     output = dataset.split_img_overlay(img)
     assert(type(output[0]) == list)
     assert(type(output[1]) == int)
